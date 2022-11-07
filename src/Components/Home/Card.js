64803a929d54551f7eeb4a50c88e53ac767bc403
sojib076 <sojibds332@gmail.com>
2022-11-07 23:58:50 +0600

    photviewer

--- src/Components/Home/Card.js
+++ src/Components/Home/Card.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const Card = ({product}) => {
-    const {name,description,price,_id,img}=product
+const Card = ({ product }) => {
+    const { name, description, price, _id, img } = product
     return (
         <div>
             <div className="card lg:w-96 w-50 lg:h-[300px]shadow-xl bg-cyan-600 mt-10">
-                <figure className="px-10 pt-10">
-                    <img src={img} alt="Shoes" className=" w-[300px] h-[170px]  lg:w-[400px] lg:h-[180px]" />
+                <figure className="px-10 pt-10">     
+                    <PhotoProvider>
+                        <PhotoView src={img}>
+                        <img src={img} alt={name} className=" w-[300px] h-[170px]  lg:w-[400px] lg:h-[180px]" />
+                        </PhotoView>
+                    </PhotoProvider>
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{name}</h2>
                     <p className="card-text"> ${price}</p>
-                    <p>{description.slice(0,100)}...</p>
+                    <p>{description.slice(0, 100)}...</p>
 
                     <div className="card-actions">
-                       <Link to={`/details/${_id}`} className='btn btn-primary'> Details</Link>
+                        <Link to={`/details/${_id}`} className='btn btn-primary'> Details</Link>
                     </div>
                 </div>
             </div>
