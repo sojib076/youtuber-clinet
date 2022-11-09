f4a04e4a0c9179ca704703af60bb45a3c737272e
sojib076 <sojibds332@gmail.com>
2022-11-09 23:45:49 +0600

    fixErr

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import error from'../video/Feeling angry-amico.png'
 import './Error.css'
 const Error = () => {
     return (
-  
+        <div className='error flex flex-col justify-center'>
+            <h1 className='text-5xl text-center text-red-700'> Error Nothing Found </h1>
+       <Link to={'/home'}className='btn btn-error w-[80%] mx-auto' > Return Home</Link>
+        </div>
     );
 };
 
